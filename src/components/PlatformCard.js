45fb30b9c08e590563ab355e3515ef4bfecf7a37
ccheaser <gazicheaser@gmail.com>
2024-08-02 17:28:50 +0300

    0.2.0

--- src/components/PlatformCard.js
+++ src/components/PlatformCard.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './PlatformCard.css'; // Stilleri import edin
 
-const PlatformCard = ({ name }) => {
+const PlatformCard = ({ name, tokenValue }) => {
   return (
     <div className="platform-card">
       <h2>{name}</h2>
+      <p>Jeton: {tokenValue}</p>
       <button>
         <Link to={`/platform/${name.toLowerCase().replace(' ', '-')}`}>Detaylar</Link>
       </button>
--- src/pages/BranchPage.js
+++ src/pages/BranchPage.js
@@ -1,18 +1,53 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import PlatformCard from '../components/PlatformCard';
-import branchData from '../data/branchData'; // Veriyi import edin
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
 import './BranchPage.css'; // Stil dosyasını import edin
 
 const BranchPage = () => {
   const { id } = useParams();
-  const platforms = branchData[id] || []; // İlgili şubenin platform kartlarını al
+  const navigate = useNavigate();
+  const [platformData, setPlatformData] = useState([]);
+  
+  useEffect(() => {
+    const db = getDatabase();
+    const auth = getAuth();
+    const userRef = ref(db, `users/${auth.currentUser.uid}`);
+    
+    // Kullanıcı rolünü almak
+    const unsubscribeUser = onValue(userRef, (snapshot) => {
+      const data = snapshot.val();
+      if (!data || !data.role) {
+        navigate("/login"); // Kullanıcı yetkisi yoksa giriş sayfasına yönlendir
+      }
+    });
+
+    return () => unsubscribeUser();
+  }, [navigate]);
+
+  useEffect(() => {
+    const db = getDatabase();
+    
+    // Firebase referanslarını oluştur
+    const platformRefs = Array.from({ length: 11 }, (_, i) => ref(db, `para${i + 1}`));
+    
+    // Para verilerini almak
+    Promise.all(platformRefs.map(ref => 
+      new Promise((resolve) => 
+        onValue(ref, (snapshot) => resolve(snapshot.val()))
+      )
+    )).then(values => {
+      setPlatformData(values);
+    });
+
+  }, []);
 
   return (
     <div className="branch-page-container">
-      <h1 className="branch-page-heading">{id} Sayfası</h1>
-      {platforms.map((platform, index) => (
-        <PlatformCard key={index} name={platform} />
+      <h1 className="branch-page-heading">Şube {id} Sayfası</h1>
+      {platformData.map((tokenValue, index) => (
+        <PlatformCard key={index} name={`Peron ${index + 1}`} tokenValue={tokenValue} />
       ))}
     </div>
   );
